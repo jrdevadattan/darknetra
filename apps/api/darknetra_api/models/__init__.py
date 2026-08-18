4c847f5f28f440450d4bd4e17b74fb7c30446923
J R Deva Dattan <jrdevadattan2006@gmail.com>
2026-08-18 18:51:45 +0530

    feat: register evidence provenance models

--- apps/api/darknetra_api/models/__init__.py
+++ apps/api/darknetra_api/models/__init__.py
@@ -2,10 +2,23 @@ from darknetra_api.models.audit import AuditEvent
 from darknetra_api.models.auth_session import AuthSession
 from darknetra_api.models.case import Case
 from darknetra_api.models.case_membership import CaseMembership, CaseMembershipRole
+from darknetra_api.models.custody import CustodyEvent
+from darknetra_api.models.evidence import EvidenceArtifact, EvidenceDerivation
 from darknetra_api.models.job import Job
 from darknetra_api.models.user import User
 
-metadata_models = (User, AuthSession, Case, CaseMembership, CaseMembershipRole, AuditEvent, Job)
+metadata_models = (
+    User,
+    AuthSession,
+    Case,
+    CaseMembership,
+    CaseMembershipRole,
+    AuditEvent,
+    Job,
+    EvidenceArtifact,
+    EvidenceDerivation,
+    CustodyEvent,
+)
 
 __all__ = [
     "AuditEvent",
@@ -13,6 +26,9 @@ __all__ = [
     "Case",
     "CaseMembership",
     "CaseMembershipRole",
+    "CustodyEvent",
+    "EvidenceArtifact",
+    "EvidenceDerivation",
     "Job",
     "User",
     "metadata_models",
